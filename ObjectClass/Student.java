a0ccfcd5985d0dc33fdcdd7c5df04bad85ff91b7
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-09-22 18:53:14 +0800

    Update Student.java

--- ObjectClass/Student.java
+++ ObjectClass/Student.java
@@ -8,11 +8,25 @@ char grade(){
     else if(total>=50) return 'B';
     return 'F';
     }
-
+public Student(){
+    name = "N/A";
+    total = 0;
+}
+public Student(String name,int total){
+    this.name = name;
+    this.total = total;
+}  
+    
     public void setTotal(int total) {
         if(total>=0 && total<=100)
         this.total = total;
     }
+    public int getTotal(){
+        return total;
+    }
+public String toString(){
+    return name + " " + total + " " + grade();
+}
 }
 
 //package statement is always the first statement in a java file because it defines the folder structure
