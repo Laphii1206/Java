56eef15f8369fb54239f8be4b33d691aa7d26aa9
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-09-25 12:09:12 +0800

    lab

---
+++ ObjectClass/LabF1WongYewChung.java
@@ -0,0 +1,30 @@
+package ObjectClass;
+
+public class LabF1WongYewChung {
+
+public void printItem(){
+        System.out.println("Item \t\t Price(RM)");
+        System.out.println("Coca-cola \t 1.80");
+        System.out.println("Mineral water \t 1.00");
+        System.out.println("Soybean \t 2.00");
+        System.out.println("Nescafe Coffee \t 3.50");
+        System.out.println("Potato Chips \t 3.80");
+        System.out.println("Snickers \t 4.20");
+        System.out.println("Twisties \t 0.80");
+}
+
+public void printReceipt(){
+        System.out.println("Thank you. Please come again.");
+    }
+
+    public double purchaseItem(double total){
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int UserCount;
+        double balance,total,amount;
+
+    }
+}
+
