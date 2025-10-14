e41cc7d1a0e207be933539d2eee8294956aeb55e
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-10-14 17:48:07 +0800

    lll

---
+++ gg.java
@@ -0,0 +1,12 @@
+public class gg{
+    public static void main(String[] args) {
+      int[][] list = new int[10][4];
+int[] offset = {11, 13, 15, 17};
+
+
+       for (int i = 0; i < offset.length; i++) {
+    System.out.print(list[0][i] = offset[i]);
+}
+
+}
+}
